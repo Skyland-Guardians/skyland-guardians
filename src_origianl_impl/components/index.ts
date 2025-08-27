6a5e6dbabd7d47496c36ba99bd69082b0b1e4894
Kevin Gao <kevin.gao.au@gmail.com>
2025-08-27 15:31:52 +1000

    chore:remove legacy code

--- src_origianl_impl/components/index.ts
+++
@@ -1,8 +0,0 @@
-export { TopBar } from './TopBar';
-export { Sidebar } from './Sidebar';
-export { GameArea } from './GameArea';
-export { FloatingButtons } from './FloatingButtons';
-export { Modals } from './Modals';
-export { GameLayout } from './GameLayout';
-export { ParentControlPanel } from './ParentControlPanel';
-export { ParentReport } from './ParentReport';
